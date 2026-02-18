4604c988d85800686f04871b667a1f6b2933a562
Derek Chmielewski <dcmagnum101@gmail.com>
2026-02-18 07:17:18 +0000

    Try fixing this error: `Runtime ReferenceError: Can't find variable: Car

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -15,6 +15,7 @@ import {
 } from "@/components/ui/table";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
 import { 
   Search, 
   Filter, 
