d4d01a60822edf76a694f73e0596250e211662aa
Derek Chmielewski <dcmagnum101@gmail.com>
2026-02-18 08:45:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7266015203-e5837",
+  "appId": "1:844077687890:web:77b7d168f7a0edf3b86b8b",
+  "apiKey": "AIzaSyD5FqRWAIcfB_xhvgqvAmslu2qX6xbqMFE",
+  "authDomain": "studio-7266015203-e5837.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "844077687890"
+};
